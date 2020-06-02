7d43d7f99b91aed6f75380d1ec17e1d3e4ba2608
3carlitosbrown <carlos.fortunylombrana@gmail.com>
2020-06-02 14:28:57 +0200

    Change

--- TrianglePosition.py
+++ TrianglePosition.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from Transformation import *
 #hello
 '''
 INPUTS:
@@ -21,14 +22,20 @@ Position: xp, yp, zp
 h = 500 #[km]
 factorsize = 15*10**(-3) #[km]
 
+#S/C1
+lat1 = np.pi/4
+lon1 = np.pi/6
 
-spacecraft1 = np.array([0,0,500+6371])
 yaw1 = 0
 roll1 = np.pi/180*5
 pitch1 = np.pi/180*10
-n = np.array([0.5,0.1])*np.pi/180
 
 
-alpha1 = roll1+n[0] #x rotation
-beta1 = pithc1+n[1] #y rotation
-gamma1 = yaw1 #z rotation
+
+
+
+
+
+#alpha1 = roll1+n[0] #x rotation
+#beta1 = pitch1+n[1] #y rotation
+#gamma1 = yaw1 #z rotation
