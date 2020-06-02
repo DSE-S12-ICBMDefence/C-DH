1cf3f84dbebf115f3f1a35ca3fb5176ad1a0636b
3carlitosbrown <carlos.fortunylombrana@gmail.com>
2020-06-02 14:54:38 +0200

    Update TrianglePosition.py

--- TrianglePosition.py
+++ TrianglePosition.py
@@ -20,21 +20,33 @@ Position: xp, yp, zp
 
 #Inputs for FOCUS
 h = 500 #[km]
+Re = 6378 #[km]
+radius = Re+h
 factorsize = 15*10**(-3) #[km]
 
 #S/C1
 lat1 = np.pi/4
 lon1 = np.pi/6
 
+spacecraftlocation1 = radius*np.array([np.cos(lon1)*np.cos(lat1),np.sin(lon1)*np.cos(lat1),np.sin(lat1)])
+
 yaw1 = 0
 roll1 = np.pi/180*5
 pitch1 = np.pi/180*10
 
+alpha1,beta1 = [np.pi/3,np.pi/4]
+vector1 = np.array([np.sin(alpha1)*np.cos(beta1),np.sin(alpha1)*np.sin(beta1),np.cos(beta1)])
 
+#S/C2
+lat2 = np.pi/4
+lon2 = np.pi/6
 
+yaw2 = 0
+roll2 = np.pi/180*5
+pitch2 = np.pi/180*10
 
-
-
+alpha2,beta2 = [np.pi/3,np.pi/4]
+vector2 = np.array([np.sin(alpha2)*np.cos(beta2),np.sin(alpha2)*np.sin(beta2),np.cos(beta2)])
 
 #alpha1 = roll1+n[0] #x rotation
 #beta1 = pitch1+n[1] #y rotation
