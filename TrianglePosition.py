46ec4f799d81505f88f3e2b78d9e2c08d717ca8f
3carlitosbrown <carlos.fortunylombrana@gmail.com>
2020-06-02 21:15:56 +0200

    Update TrianglePosition.py

--- TrianglePosition.py
+++ TrianglePosition.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-from Transformation import *
+from Transformation import Transformation
 #hello
 '''
 INPUTS:
@@ -25,6 +25,8 @@ radius = Re+h
 factorsize = 15*10**(-3) #[km]
 
 #S/C1
+
+#Longitude and latitude 
 lat1 = np.pi/4
 lon1 = np.pi/6
 
@@ -35,19 +37,37 @@ roll1 = np.pi/180*5
 pitch1 = np.pi/180*10
 
 alpha1,beta1 = [np.pi/3,np.pi/4]
-vector1 = np.array([np.sin(alpha1)*np.cos(beta1),np.sin(alpha1)*np.sin(beta1),np.cos(beta1)])
+v1 = np.array([np.sin(alpha1),np.cos(alpha1)*np.sin(beta1),np.cos(alpha1)*np.cos(beta1)])
+vector1 = np.diagonal(Transformation(lon1,lat1,yaw1,pitch1,roll1)*v1)
+print(vector1)
 
 #S/C2
-lat2 = np.pi/4
+lat2 = np.pi/4.2
 lon2 = np.pi/6
 
+spacecraftlocation2 = radius*np.array([np.cos(lon2)*np.cos(lat2),np.sin(lon2)*np.cos(lat2),np.sin(lat2)])
+
 yaw2 = 0
-roll2 = np.pi/180*5
-pitch2 = np.pi/180*10
+roll2 = np.pi/180*7
+pitch2 = np.pi/180*12
 
-alpha2,beta2 = [np.pi/3,np.pi/4]
-vector2 = np.array([np.sin(alpha2)*np.cos(beta2),np.sin(alpha2)*np.sin(beta2),np.cos(beta2)])
+alpha2,beta2 = [np.pi/3,-np.pi/4]
+v2 = np.array([np.sin(alpha2),np.cos(alpha2)*np.sin(beta2),np.cos(alpha2)*np.cos(beta2)])
+vector2 = np.diagonal(Transformation(lon2,lat2,yaw2,pitch2,roll2)*v2)
 
-#alpha1 = roll1+n[0] #x rotation
-#beta1 = pitch1+n[1] #y rotation
-#gamma1 = yaw1 #z rotation
+#Nearest points
+n = np.cross(vector1,vector2)
+n1 = np.cross(vector1,n)
+n2 = np.cross(vector2,n)
+
+point1 = spacecraftlocation1+(np.dot((spacecraftlocation2-spacecraftlocation1),n2)/np.dot(vector1,n2))*vector1
+point2 = spacecraftlocation2+(np.dot((spacecraftlocation1-spacecraftlocation2),n1)/np.dot(vector2,n1))*vector2
+
+print(point1,point2)
+print(np.dot((spacecraftlocation1-spacecraftlocation2),n1)/np.dot(vector2,n1))
+
+#Distance for Error
+n_normalized = n/np.linalg.norm(n)
+distance1 = np.abs(np.dot(n_normalized,(spacecraftlocation2-spacecraftlocation1)))
+distance2 = np.linalg.norm(point2-point1)
+print(distance1,distance2)
