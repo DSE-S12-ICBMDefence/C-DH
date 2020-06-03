8262070fbf778fd2bd0eebce5d1ce3c524a6a48a
3carlitosbrown <carlos.fortunylombrana@gmail.com>
2020-06-03 12:04:06 +0200

    Update TrianglePosition.py

--- TrianglePosition.py
+++ TrianglePosition.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-from Transformation import Transformation
+from Transformation import TransformationOrbitalEarth
 '''
 INPUTS:
 Spacecraft 1 (S/C1)
@@ -26,49 +26,53 @@ factorsize = 15*10**(-3) #[km]
 ###S/C1###
 
 #Longitude and latitude 
-lat1 = np.pi/4 #[rad]
-lon1 = np.pi/6 #[rad]
+lat1 = np.pi/2 #[rad]
+lon1 = np.pi/2 #[rad]
 
 #S/C1 position in km
 spacecraftlocation1 = radius*np.array([np.cos(lon1)*np.cos(lat1),np.sin(lon1)*np.cos(lat1),np.sin(lat1)])
 
 #Attutide angles
 yaw1 = 0 #[rad]
-roll1 = np.pi/180*5 #[rad]
-pitch1 = np.pi/180*10 #[rad]
+roll1 = 0 #[rad]
+pitch1 = 0 #[rad]
 
 #Angles of the bright pixel in radians
-alpha1,beta1 = [np.pi/3,np.pi/4]
+alpha1,beta1 = [0,0]
 
 #Representation of vector in 3D using the two angles given (alpha and beta)
 v1 = np.array([np.sin(alpha1),np.cos(alpha1)*np.sin(beta1),np.cos(alpha1)*np.cos(beta1)])
 
 #Transformation of vector into Earth fixed coordinate system
-vector1 = np.diagonal(Transformation(lon1,lat1,yaw1,pitch1,roll1)*v1)
+vector1 = np.diagonal(TransformationOrbitalEarth(yaw1,pitch1,roll1)*v1)
+print(v1)
 
 ###S/C2###
 
 #Longitude and latitude
-lat2 = np.pi/4.2 #[rad]
-lon2 = np.pi/6 #[rad]
+lat2 = np.pi/180*80 #[rad]
+lon2 = np.pi/2 #[rad]
 
 
 #S/C2 position in km
 spacecraftlocation2 = radius*np.array([np.cos(lon2)*np.cos(lat2),np.sin(lon2)*np.cos(lat2),np.sin(lat2)])
 
+print(np.linalg.norm(spacecraftlocation2-spacecraftlocation1))
+
 #Attitude angles
 yaw2 = 0 #[rad]
-roll2 = np.pi/180*7 #[rad]
-pitch2 = np.pi/180*12 #[rad]
+roll2 = -np.pi/180*58 #[rad]
+pitch2 = 0 #[rad]
 
 #Angles of the bright pixel in radians
-alpha2,beta2 = [np.pi/3,-np.pi/4]
+alpha2,beta2 = [0,0]
 
 #Representation of vector in #D using the two angles given (alpha and beta)
 v2 = np.array([np.sin(alpha2),np.cos(alpha2)*np.sin(beta2),np.cos(alpha2)*np.cos(beta2)])
 
 #Transformation of vector into Earth fixed coordinate system
-vector2 = np.diagonal(Transformation(lon2,lat2,yaw2,pitch2,roll2)*v2)
+vector2 = np.diagonal(TransformationOrbitalEarth(yaw2,pitch2,roll2)*v2)
+print(vector2)
 
 #Nearest points
 n = np.cross(vector1,vector2)
