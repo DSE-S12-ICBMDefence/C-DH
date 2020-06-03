1c990b1b57f41c342e8c191674264976978e3453
3carlitosbrown <carlos.fortunylombrana@gmail.com>
2020-06-03 11:30:09 +0200

    Update TrianglePosition.py

--- TrianglePosition.py
+++ TrianglePosition.py
@@ -18,41 +18,57 @@ OUTPUTS:
 Position: xp, yp, zp
 '''
 
-#Inputs for FOCUS
+#Inputs for FOCUS payload
 h = 500 #[km]
 Re = 6378 #[km]
-radius = Re+h
+radius = Re+h #Sum between earth radius and altitude
 factorsize = 15*10**(-3) #[km]
 
-#S/C1
+###S/C1###
 
 #Longitude and latitude 
-lat1 = np.pi/4
-lon1 = np.pi/6
+lat1 = np.pi/4 #[rad]
+lon1 = np.pi/6 #[rad]
 
+#S/C1 position in km
 spacecraftlocation1 = radius*np.array([np.cos(lon1)*np.cos(lat1),np.sin(lon1)*np.cos(lat1),np.sin(lat1)])
 
-yaw1 = 0
-roll1 = np.pi/180*5
-pitch1 = np.pi/180*10
+#Attutide angles
+yaw1 = 0 #[rad]
+roll1 = np.pi/180*5 #[rad]
+pitch1 = np.pi/180*10 #[rad]
 
+#Angles of the bright pixel in radians
 alpha1,beta1 = [np.pi/3,np.pi/4]
+
+#Representation of vector in 3D using the two angles given (alpha and beta)
 v1 = np.array([np.sin(alpha1),np.cos(alpha1)*np.sin(beta1),np.cos(alpha1)*np.cos(beta1)])
+
+#Transformation of vector into Earth fixed coordinate system
 vector1 = np.diagonal(Transformation(lon1,lat1,yaw1,pitch1,roll1)*v1)
-print(vector1)
 
-#S/C2
-lat2 = np.pi/4.2
-lon2 = np.pi/6
+###S/C2###
 
+#Longitude and latitude
+lat2 = np.pi/4.2 #[rad]
+lon2 = np.pi/6 #[rad]
+
+
+#S/C2 position in km
 spacecraftlocation2 = radius*np.array([np.cos(lon2)*np.cos(lat2),np.sin(lon2)*np.cos(lat2),np.sin(lat2)])
 
-yaw2 = 0
-roll2 = np.pi/180*7
-pitch2 = np.pi/180*12
+#Attitude angles
+yaw2 = 0 #[rad]
+roll2 = np.pi/180*7 #[rad]
+pitch2 = np.pi/180*12 #[rad]
 
+#Angles of the bright pixel in radians
 alpha2,beta2 = [np.pi/3,-np.pi/4]
+
+#Representation of vector in #D using the two angles given (alpha and beta)
 v2 = np.array([np.sin(alpha2),np.cos(alpha2)*np.sin(beta2),np.cos(alpha2)*np.cos(beta2)])
+
+#Transformation of vector into Earth fixed coordinate system
 vector2 = np.diagonal(Transformation(lon2,lat2,yaw2,pitch2,roll2)*v2)
 
 #Nearest points
