e859804883723bacd4f6122327b191386b2184a2
3carlitosbrown <carlos.fortunylombrana@gmail.com>
2020-06-04 16:18:05 +0200

    Update TrianglePosition.py

--- TrianglePosition.py
+++ TrianglePosition.py
@@ -31,7 +31,7 @@ lon1 = 0 #[rad]
 
 #S/C1 position in km
 spacecraftlocation1 = radius*np.array([np.cos(lon1)*np.cos(lat1),np.sin(lon1)*np.cos(lat1),np.sin(lat1)])
-print("S/C1 Location: ",spacecraftlocation1)
+print("S/C1 Location [km]: ",spacecraftlocation1)
 
 #Attutide angles
 yaw1 = 0 #[rad]
@@ -43,12 +43,12 @@ alpha1,beta1 = [0,0]
 
 #Representation of vector in 3D using the two angles given (alpha and beta)
 v1 = np.array([np.sin(alpha1),np.cos(alpha1)*np.sin(beta1),np.cos(alpha1)*np.cos(beta1)])
-print("Vector1 Camera:",v1)
+print("Vector1 Camera: ",v1)
 
 #Transformation of vector into Earth fixed coordinate system
 vector1 = np.dot(TransformationOrbitalEarth(yaw1,pitch1,roll1),v1)
 
-print("Vector1 Attitude:",vector1,'\n')
+print("Vector1 Attitude: ",vector1,'\n')
 
 ###S/C2###
 
@@ -58,23 +58,25 @@ lon2 = 0 #[rad]
 
 #S/C2 position in km
 spacecraftlocation2 = radius*np.array([np.cos(lon2)*np.cos(lat2),np.sin(lon2)*np.cos(lat2),np.sin(lat2)])
-print("S/C2 Location: ",spacecraftlocation2)
+print("S/C2 Location [km]: ",spacecraftlocation2)
 
 #Attitude angles
 yaw2 = 0 #[rad]
 roll2 = 0 #[rad]
-pitch2 = np.pi/180*170 #[rad]
+pitch2 = np.pi/180*(90+18.32) #[rad] #Test 170deg
 
 #Angles of the bright pixel in radians
 alpha2,beta2 = [0,0]
 
 #Representation of vector in #D using the two angles given (alpha and beta)
 v2 = np.array([np.sin(alpha2),np.cos(alpha2)*np.sin(beta2),np.cos(alpha2)*np.cos(beta2)])
-print("Vector2 Camera",v2)
+print("Vector2 Camera: ",v2)
 
 #Transformation of vector into Earth fixed coordinate system
 vector2 = np.dot(TransformationOrbitalEarth(yaw2,pitch2,roll2),v2)
-print("Vector2 Attitude",vector2,'\n')
+print("Vector2 Attitude: ",vector2,'\n')
+
+print("Distance between location S/C1 and S/C2 [km]: ",np.linalg.norm(spacecraftlocation1-spacecraftlocation2))
 
 #Nearest points
 n = np.cross(vector1,vector2)
@@ -95,4 +97,4 @@ print("Point2: ",point2)
 n_normalized = n/np.linalg.norm(n)
 distance1 = np.abs(np.dot(n_normalized,(spacecraftlocation2-spacecraftlocation1)))
 distance2 = np.linalg.norm(point2-point1)
-print("Distance between Point1 and Point2: ",distance1,"or ",distance2)
+print("Distance between Point1 and Point2 [km]: ",distance1,"or ",distance2)
