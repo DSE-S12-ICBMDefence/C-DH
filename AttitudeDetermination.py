731645e7bf4c9fa4d9e162a496854bd6d936deb8
3carlitosbrown <carlos.fortunylombrana@gmail.com>
2020-06-04 15:19:41 +0200

    Update AttitudeDetermination.py

--- AttitudeDetermination.py
+++ AttitudeDetermination.py
@@ -2,6 +2,8 @@ import math
 import numpy as np
 
 #Mass in kg
+
+#mass = 19.94 #[kg] For CATIA
 mass = 24 #[kg]
 
 #Dimensions of the 12U
@@ -14,6 +16,23 @@ I = np.array([[1/12*mass*(height**2+depth**2),0,0],   #Ixx
               [0,1/12*mass*(width**2+depth**2),0],    #Iyy
               [0,0,1/12*mass*(width**2+height**2)]])  #Izz
 
+print("MOI (just structure): ",I)
+
+##Moments of Inertia for 12U Cubesat with 2 solar panels attached in the width of the cubesat
+#Moments of inertia with solar array
+
+#mass_solarpanel = 0.69 #[kg] for CATIA
+mass_solarpanel = 0.728 #[kg]
+area_solarpanel = 0.23 #[m^2]
+thickness_solarpanel = 3*10**(-3) #[m]
+height_solarpanel = area_solarpanel/2/width
+
+I_solarpanel = np.array([[1/12*mass*(height**2+depth**2)+1/12*mass_solarpanel*(height_solarpanel**2+thickness_solarpanel**2)+mass_solarpanel*((height_solarpanel/2+height/2)**2+(depth/2-thickness_solarpanel/2)**2),0,0],   #Ixx
+              [0,1/12*mass*(width**2+depth**2)+1/12*mass_solarpanel*(width**2+thickness_solarpanel**2)+mass_solarpanel*((depth/2-thickness_solarpanel/2)**2),0],    #Iyy
+              [0,0,1/12*mass*(width**2+height**2)+1/12*mass_solarpanel*(width**2+height_solarpanel**2)+mass_solarpanel*(height_solarpanel/2+height/2)**2]])  #Izz
+
+print("MOI including solar arrays: ",I_solarpanel)
+
 #alpha around y
 #beta around x
 
@@ -24,10 +43,10 @@ def AttitudeDeterminationTime(Ixx,Iyy,Torque,alpha,beta):
     time = time1 + time2
     return time
 
-Ixx = I[0][0]
-Iyy = I[1][1]
+Ixx = I_solarpanel[0][0]
+Iyy = I_solarpanel[1][1]
 Torque = 0.007 #[Nm]
 alpha = np.pi/180*20 #[rad]
 beta = np.pi/180*50 #[rad]
 
-print(AttitudeDeterminationTime(Ixx,Iyy,Torque,alpha,beta))
+print("Total Time: ",AttitudeDeterminationTime(Ixx,Iyy,Torque,alpha,beta))
