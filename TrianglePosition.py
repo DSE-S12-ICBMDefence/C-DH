7e99a1d4e8d8f2eb12745f631966e292f166f226
3carlitosbrown <carlos.fortunylombrana@gmail.com>
2020-06-08 10:36:31 +0200

    Debug

--- TrianglePosition.py
+++ TrianglePosition.py
@@ -6,12 +6,12 @@ import time
 INPUTS:
 Spacecraft 1 (S/C1)
 Attitude: yaw1, roll1, pitch1 [rad] #Order Z->X->Y
-Position: x1. y1. z1 [km]
+Position: lat1,lon1,h1
 Bright pixel: alpha1 (around y axis), beta1 (around x axis) [rad]
 
 Spacecraft 2 (S/C2)
 Attitude: yaw2, roll2, pitch2 
-Position: x2, y2, z2
+Position: lat2,lon2,h2
 Bright pixel: alpha2 (around y axis), beta2 (around x axis) [rad]
 
 OUTPUTS:
@@ -22,92 +22,97 @@ Distance between Position1 and Position2: d
 FinalPoint: xp, yp, zp 
 '''
 
-start_time = time.time()
 #Inputs for FOCUS payload
 h = 500 #[km]
-Re = 6378 #[km]
-radius = Re+h #Sum between earth radius and altitude
 
 ###S/C1###
-
-#Longitude and latitude 
+#Longitude and latitude
 lat1 = np.pi/2 #[rad]
 lon1 = 0 #[rad]
-
-#S/C1 position in km
-spacecraftlocation1 = radius*np.array([np.cos(lon1)*np.cos(lat1),np.sin(lon1)*np.cos(lat1),np.sin(lat1)])
-print("S/C1 Location [km]: ",spacecraftlocation1)
-
 #Attutide angles
 yaw1 = np.pi/4 #[rad]
 roll1 = 0 #[rad]
 pitch1 = np.pi #[rad]
 
 #Angles of the bright pixel in radians
-alpha1,beta1 = [0,0]
-
-#Representation of vector in 3D using the two angles given (alpha and beta)
-v1 = np.array([np.sin(alpha1),np.cos(alpha1)*np.sin(beta1),np.cos(alpha1)*np.cos(beta1)])
-print("Vector1 Camera: ",v1)
-
-#Transformation of vector into Earth fixed coordinate system
-vector1 = np.dot(TransformationOrbitalEarth(yaw1,pitch1,roll1),v1)
-
-print("Vector1 Attitude: ",vector1,'\n')
+alpha1 = 0 #[rad]
+beta1 = 0 #[rad]
 
 ###S/C2###
-
 #Longitude and latitude
 lat2 = np.pi/180*68.018 #[rad]
 lon2 = np.pi/2 #[rad]
 
-#S/C2 position in km
-spacecraftlocation2 = radius*np.array([np.cos(lon2)*np.cos(lat2),np.sin(lon2)*np.cos(lat2),np.sin(lat2)])
-print("S/C2 Location [km]: ",spacecraftlocation2)
-
 #Attitude angles
 yaw2 = np.pi #[rad]
 roll2 = np.pi/180*(180-68.018) #[rad]
-pitch2 = 0 #(90+18.32) #[rad] #Test 170deg
+pitch2 = 0 #[rad]     (90+18.32) #[rad] #Test 170deg
 
 #Angles of the bright pixel in radians
-alpha2,beta2 = [0,np.pi/180*38.018]
+alpha2 = 0 #[rad]
+beta2 = np.pi/180*38.018 #[rad]
 
-#Representation of vector in #D using the two angles given (alpha and beta)
-v2 = np.array([np.sin(alpha2),np.cos(alpha2)*np.sin(beta2),np.cos(alpha2)*np.cos(beta2)])
-print("Vector2 Camera: ",v2)
+def TrianglePosition(h,lat1,lon1,yaw1,roll1,pitch1,alpha1,beta1,lat2,lon2,yaw2,roll2,pitch2,alpha2,beta2):
+    start_time = time.time()
 
-#Transformation of vector into Earth fixed coordinate system
-vector2 = np.dot(TransformationOrbitalEarth(yaw2,pitch2,roll2),v2)
+    Re = 6378 #[km]
+    radius = Re+h #Sum between earth radius and altitude
 
-print("Vector2 Attitude: ",vector2,'\n')
+    #S/C1 position in km
+    spacecraftlocation1 = radius*np.array([np.cos(lon1)*np.cos(lat1),np.sin(lon1)*np.cos(lat1),np.sin(lat1)])
+    print("S/C1 Location [km]: ",spacecraftlocation1)
 
-print("Distance between location S/C1 and S/C2 [km]: ",np.linalg.norm(spacecraftlocation1-spacecraftlocation2),'\n')
+    #Representation of vector in 3D using the two angles given (alpha and beta)
+    v1 = np.array([np.sin(alpha1),np.cos(alpha1)*np.sin(beta1),np.cos(alpha1)*np.cos(beta1)])
+    print("Vector1 Camera: ",v1)
 
-#Nearest points
-n = np.cross(vector1,vector2)
-n1 = np.cross(vector1,n)
-n2 = np.cross(vector2,n)
+    #Transformation of vector into Earth fixed coordinate system
+    vector1 = np.dot(TransformationOrbitalEarth(yaw1,pitch1,roll1),v1)
 
-#Nearest point for the line of the S/C1
-point1 = spacecraftlocation1+(np.dot((spacecraftlocation2-spacecraftlocation1),n2)/np.dot(vector1,n2))*vector1
+    print("Vector1 Attitude: ",vector1,'\n')
 
-#Nearest point for the line of the S/C2
-point2 = spacecraftlocation2+(np.dot((spacecraftlocation1-spacecraftlocation2),n1)/np.dot(vector2,n1))*vector2
+    #S/C2 position in km
+    spacecraftlocation2 = radius*np.array([np.cos(lon2)*np.cos(lat2),np.sin(lon2)*np.cos(lat2),np.sin(lat2)])
+    print("S/C2 Location [km]: ",spacecraftlocation2)
 
-#Printing the two points and the
-print("Point1 [km]: ",point1)
-print("Point2 [km]: ",point2)
+    #Representation of vector in #D using the two angles given (alpha and beta)
+    v2 = np.array([np.sin(alpha2),np.cos(alpha2)*np.sin(beta2),np.cos(alpha2)*np.cos(beta2)])
+    print("Vector2 Camera: ",v2)
 
-#Distance for Error
-n_normalized = n/np.linalg.norm(n)
-distance1 = np.abs(np.dot(n_normalized,(spacecraftlocation2-spacecraftlocation1)))
-distance2 = np.linalg.norm(point2-point1)
-print("Distance between Point1 and Point2 [km]: ",distance1,"or ",distance2,'\n')
+    #Transformation of vector into Earth fixed coordinate system
+    vector2 = np.dot(TransformationOrbitalEarth(yaw2,pitch2,roll2),v2)
 
-final_point = (point1+point2)/2
-print("Final Point [km]:",final_point,'\n')
-print("--- Computational Time: %s seconds ---" % (time.time() - start_time))
+    print("Vector2 Attitude: ",vector2,'\n')
+
+    print("Distance between location S/C1 and S/C2 [km]: ",np.linalg.norm(spacecraftlocation1-spacecraftlocation2),'\n')
+
+    #Nearest points
+    n = np.cross(vector1,vector2)
+    n1 = np.cross(vector1,n)
+    n2 = np.cross(vector2,n)
+
+    #Nearest point for the line of the S/C1
+    point1 = spacecraftlocation1+(np.dot((spacecraftlocation2-spacecraftlocation1),n2)/np.dot(vector1,n2))*vector1
+
+    #Nearest point for the line of the S/C2
+    point2 = spacecraftlocation2+(np.dot((spacecraftlocation1-spacecraftlocation2),n1)/np.dot(vector2,n1))*vector2
+
+    #Printing the two points and the
+    print("Point1 [km]: ",point1)
+    print("Point2 [km]: ",point2)
+
+    #Distance for Error
+    n_normalized = n/np.linalg.norm(n)
+    distance1 = np.abs(np.dot(n_normalized,(spacecraftlocation2-spacecraftlocation1)))
+    distance2 = np.linalg.norm(point2-point1)
+    print("Distance between Point1 and Point2 [km]: ",distance1,"or ",distance2,'\n')
+
+    final_point = (point1+point2)/2
+    print("Final Point [km]:",final_point,'\n')
+    print("--- Computational Time: %s seconds ---" % (time.time() - start_time))
+    return final_point
+
+print(TrianglePosition(h,lat1,lon1,yaw1,roll1,pitch1,alpha1,beta1,lat2,lon2,yaw2,roll2,pitch2,alpha2,beta2))
 
 #%%
 
