0bfdc1e2b63dc87b1230faafd21036f3002a1852
sboby <56260669+sboby@users.noreply.github.com>
2020-06-08 10:23:23 +0200

    Sara and Mahima pt1

---
+++ IntegratingCDH.py
@@ -0,0 +1,15 @@
+import numpy as np
+import math as m
+import matplotlib.pyplot as plt
+import scipy as sp
+from scipy import interpolate
+from AverageThrust import AvgThurst
+from CdEstimator import Cd
+#from ISA_2 import mass_time
+from ISA_2 import density_at_height
+from TempAlt import  Temp
+from Mass_extract_2 import Mass_t
+from Velocity_Check import pixel_det
+from Velocity_Check import pixel_data
+from Main_Trajectory import TrajectoryData
+
